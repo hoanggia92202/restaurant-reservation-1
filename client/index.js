ee9c561e362fef0b3b3d7b4eaed899f68d2221e2
duc le <ducle@ducs-MacBook-Pro.local>
2022-08-21 01:32:38 -0700

    add support for react 18.

--- client/index.js
+++ client/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App/App";
 
+/*
 ReactDOM.render(
   <React.StrictMode>
       <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
